d0fa27d98af324573d8b7bbc813fb8e8fa35c90d
Wilson <IWILSON001@CSBSJU.EDU>
2021-10-20 23:12:25 -0500

    Login Page Up
    -Login page has been coded
    -Doesn't work, but HTML exists

--- DormDashApp/urls.py
+++ DormDashApp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # URLConf
 urlpatterns = [
     path('createaccount/', views.createaccount),
     path('', views.login),
 
 ]
+urlpatterns += staticfiles_urlpatterns()
